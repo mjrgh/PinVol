20a08b7745af848ee16b018b721342ec901f2108
mjrgh <mjr_@hotmail.com>
2025-10-08 15:43:20 -0700

    update copyright dates

--- PinVol/Properties/AssemblyInfo.cs
+++ PinVol/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PinVol")]
-[assembly: AssemblyCopyright("Copyright 2016-2020, Michael J Roberts; MIT License")]
+[assembly: AssemblyCopyright("Copyright 2016-2025, Michael J Roberts; MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
